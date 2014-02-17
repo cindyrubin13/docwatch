bb073bf6c19b35f0ddeaecf6e2fc4f416cdd9d24
cindyrubin <cindyrubin13@gmail.com>
2014-02-17 12:24:54 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FindDoc::Application.config.secret_key_base = '8b6d909cd4e7dc2c02179ca129a7c286f09763e71965c9375b46b17db5f7e511dd19ca4619678bb391049811558526ad378f2153ededf3cd92546bd9e4e099ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FindDoc::Application.config.session_store :cookie_store, key: '_FindDoc_session'
